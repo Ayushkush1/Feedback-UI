c886f24a66300dd86c97455f79db79b00e64b9ea
Webby Ayush <114153125+Ayushkush1@users.noreply.github.com>
2023-02-07 19:08:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const ratingEls = document.querySelectorAll(".rating");
+const btnEl = document.getElementById("btn");
+
+const containerEl = document.getElementById("container");
+
+let selectedRating = "";
+
+
+
+
+
+ratingEls.forEach((ratingEl) => {
+    ratingEl.addEventListener("click", (event) => {
+        removeActive();
+        selectedRating =
+            event.target.innerText || event.target.parentNode.innerText;
+        event.target.classList.add("active");
+        event.target.parentNode.classList.add("active");
+
+    });
+});
+
+btnEl.addEventListener("click", () => {
+    if(selectedRating !== ""){
+          containerEl.innerHTML = `
+          <strong class="jshtml">Thank You!</strong>
+          <br>
+          <br>
+          <strong class="jshtml1">Feedback: ${selectedRating}</strong>
+          <p class="jshtml2">We'll use your feedback to improve our customer support.</p>
+          `
+    }
+})
+
+
+function removeActive() {
+    ratingEls.forEach((ratingEl) => {
+        ratingEl.classList.remove("active");
+    });
+};
